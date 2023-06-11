348145fe33e466e09659a16a1820c026ed8af6c7
anmcfarl963 <74265175+anmcfarl963@users.noreply.github.com>
2023-06-11 19:01:15 -0400

    Update books.js

--- books.js
+++ books.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+class Book {
+    constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
